a05fd3a4f992d308dd99a95fed16b7787bf47352
DevPBDias <devpbdias@gmail.com>
2024-10-28 15:36:56 -0300

    feat(fonts): add anton and roboyo fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,21 @@
 import type { Metadata } from "next";
+import { Anton, Roboto } from "next/font/google";
 import "./globals.css";
+
+const anton = Anton({
+  subsets: ["latin", "latin-ext", "vietnamese"],
+  weight: "400",
+  display: "swap",
+  variable: "--font-anton",
+});
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${anton.variable} ${roboto.variable}`}>
       <body>{children}</body>
     </html>
   );
