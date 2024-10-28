5045a5587ae57cbfa1c82e27fdb5731a195d6680
DevPBDias <devpbdias@gmail.com>
2024-10-28 15:41:48 -0300

    feat(container): add container component

--- src/app/gym/page.tsx
+++ src/app/gym/page.tsx
@@ -1,3 +1,7 @@
-export default function GymWebsite() {
-  return <h1>Landing Page Gym</h1>;
+import React from "react";
+
+export default function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="w-full px-12 border-red-600 border-2">{children}</main>
+  );
 }
