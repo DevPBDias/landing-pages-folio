56680248a4e965dda468d4fda2175e4a2280c1f1
DevPBDias <devpbdias@gmail.com>
2024-11-06 10:30:53 -0300

    feat(video): create video components

--- src/app/farm/page.tsx
+++ src/app/farm/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/farm/about/About";
 import Header from "@/components/farm/header/Header";
 import Hero from "@/components/farm/hero/Hero";
 import Products from "@/components/farm/products/Products";
+import FarmVideo from "@/components/farm/video/FarmVideo";
 
 export default function FarmWbesite() {
   return (
@@ -10,6 +11,7 @@ export default function FarmWbesite() {
       <Hero />
       <About />
       <Products />
+      <FarmVideo />
     </main>
   );
 }
---
+++ src/components/farm/video/FarmVideo.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import playBtn from "@/assets/farm/icons/play btn 3.png";
+import video from "@/assets/farm/images/video.png";
+import Link from "next/link";
+
+const FarmVideo = () => {
+  return (
+    <section className="w-full my-12 h-[70vh] flex relative justify-center items-center">
+      <picture className="w-full h-full">
+        <Image
+          src={video}
+          alt="hero image"
+          className="object-cover w-full h-full"
+          quality={100}
+          priority
+        />
+      </picture>
+      <div className="absolute flex flex-col justify-center items-center px-4 gap-4">
+        <h1 className="w-2/3 text-center">
+          Agriculture Matters to the Future of Bangladesh
+        </h1>
+        <Link
+          href="https://www.youtube.com/watch?v=5ZiHkYhWEto&t=4s"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex justify-center items-center"
+        >
+          <Image
+            src={playBtn}
+            alt="play icon"
+            quality={100}
+            priority
+            className="w-full h-full object-cover"
+          />
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default FarmVideo;
